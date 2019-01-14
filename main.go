9c3878b337d77e2c9bf2dcb03adf1c252be6751f
iffigues <iffigues@vivaldi.net>
2019-01-14 21:20:29 +0100

    eee

--- main.go
+++ main.go
@@ -9,11 +9,23 @@ type Widget struct {
 	Id string
 }
 
-func MakeWidget(Doc *js.Object,Elem , Id string)(w *Widget){
-	w.Doc = Doc
+func MakeWidget(Doc, Elem, Id string)(w *Widget){
+	w.Doc = js.Global.Get(Doc)
 	w.Elem = Elem
 	w.Id = Id
 	return w
+}
+
+func (w *Widget)Twttr() {
+	var t *js.Object
+	window := js.Global.Get("window")
+	fjs := w.Doc.Call("getElementByTagName", w.Elem).Index(0)
+	if window.Get("twttr") != nil{
+		t =  window.Get("twttr")
+	}
+	js := w.Doc.Call("getElementById",w.Id)
+	js.Set("src","https://platform.twitter.com/widgets.js")
+	
 }
 
 func main() {
